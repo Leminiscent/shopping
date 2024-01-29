f391940832c2f7c5316f6834779f306fcca25c9d
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-01-29 09:39:58 -0500

    Refactor load_data function to read CSV file and convert data into evidence and labels lists

--- shopping.py
+++ shopping.py
@@ -8,7 +8,6 @@ TEST_SIZE = 0.4
 
 
 def main():
-
     # Check command-line arguments
     if len(sys.argv) != 2:
         sys.exit("Usage: python shopping.py data")
@@ -32,34 +31,36 @@ def main():
 
 
 def load_data(filename):
-    """
-    Load shopping data from a CSV file `filename` and convert into a list of
-    evidence lists and a list of labels. Return a tuple (evidence, labels).
+    with open(filename) as f:
+        reader = csv.reader(f)
+        next(reader)  # Skip the header row
 
-    evidence should be a list of lists, where each list contains the
-    following values, in order:
-        - Administrative, an integer
-        - Administrative_Duration, a floating point number
-        - Informational, an integer
-        - Informational_Duration, a floating point number
-        - ProductRelated, an integer
-        - ProductRelated_Duration, a floating point number
-        - BounceRates, a floating point number
-        - ExitRates, a floating point number
-        - PageValues, a floating point number
-        - SpecialDay, a floating point number
-        - Month, an index from 0 (January) to 11 (December)
-        - OperatingSystems, an integer
-        - Browser, an integer
-        - Region, an integer
-        - TrafficType, an integer
-        - VisitorType, an integer 0 (not returning) or 1 (returning)
-        - Weekend, an integer 0 (if false) or 1 (if true)
-
-    labels should be the corresponding list of labels, where each label
-    is 1 if Revenue is true, and 0 otherwise.
-    """
-    raise NotImplementedError
+        evidence = []
+        labels = []
+        for row in reader:
+            evidence.append(
+                [
+                    int(row[0]),  # Administrative
+                    float(row[1]),  # Administrative_Duration
+                    int(row[2]),  # Informational
+                    float(row[3]),  # Informational_Duration
+                    int(row[4]),  # ProductRelated
+                    float(row[5]),  # ProductRelated_Duration
+                    float(row[6]),  # BounceRates
+                    float(row[7]),  # ExitRates
+                    float(row[8]),  # PageValues
+                    float(row[9]),  # SpecialDay
+                    get_month(row[10]),  # Month
+                    int(row[11]),  # OperatingSystems
+                    int(row[12]),  # Browser
+                    int(row[13]),  # Region
+                    int(row[14]),  # TrafficType
+                    get_visitor_type(row[15]),  # VisitorType
+                    get_weekend(row[16]),  # Weekend
+                ]
+            )
+            labels.append(int(row[17]))  # Label
+        return evidence, labels
 
 
 def train_model(evidence, labels):
